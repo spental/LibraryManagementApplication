488229023d348d0551fdfa266261f7e591a8f2e2
Amita <panwar.amita@gmail.com>
2021-03-06 11:00:01 -0500

    added the new folder

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require("express");
 const session = require("express-session");
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
-
+const config=require("config");
 // Setting up port and requiring models for syncing
-const PORT = process.env.PORT || 8080;
+const PORT = config.PORT;
 const db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
