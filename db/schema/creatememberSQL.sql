18b055b57e9c26ce8db0d2622ac368f1a57d74d2
Leslie Book <lesliek.book@gmail.com>
2021-03-09 19:35:02 -0500

    Update creatememberSQL.sql

--- db/schema/creatememberSQL.sql
+++ db/schema/creatememberSQL.sql
@@ -12,7 +12,7 @@ CREATE TABLE members (
   id int NOT NULL AUTO_INCREMENT,
   memID varchar(50) NOT NULL,
   email varchar(50) NOT NULL,
-  phoneno int NOT NULL,
+  phoneno BIGINT NOT NULL,
   firstName varchar(30) NOT NULL,
   lastName varchar(50) NOT NULL,
   createdAt timestamp NOT NULL,
